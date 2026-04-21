34eeb72f844b54cc2b86d39eb85d66d681ee9158
Missy <mhyatt91@yahoo.com>
2026-04-21 10:21:10 -0400

    Cloned Repo and transferred files Sprint 12

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const User = require("../models/user");
+
+// GET /users
+
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(200).send(users))
+    .catch((err) => {
+      console.error(err);
+      return res.status(500).send({ message: err.message });
+    });
+};
+
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar }).then((user) => res.status(201).send(user));
+  //console.error(err);
+  //console.log(err.name);
+  //return res.status(500).send({ message: err.message });
+};
+
+module.exports = { getUsers, createUser };
---
+++ models/clothingItems.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const clothingItemSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, maxlength: 30 },
+  avatar: {
+    type: String,
+    required: true,
+  },
+});
+
+module.export = mongoose.model("user", clothingItemSchema);
