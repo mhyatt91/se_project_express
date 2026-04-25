f3e432400f80938b568910990d7398591d8717a2
Missy <mhyatt91@yahoo.com>
2026-04-25 10:09:02 -0400

    Sprint 12 Errors imported

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,7 @@
 const User = require("../models/user");
+const { INTERNAL_SERVER_ERROR } = require("../utils/errors");
+const { BAD_REQUEST_ERROR } = require("../utils/errors");
+const { NOT_FOUND } = require("../utils/errors");
 
 // GET /users
 
