91ee798dd707149fb38137f91119062b8911b1d7
Missy <mhyatt91@yahoo.com>
2026-05-18 22:11:55 -0400

    routes corrected /users removed

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,12 @@ const { login, createUser } = require("../controllers/users");
 const clothingItems = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
 const userRouter = require("./users");
-const users = require("./users");
 const auth = require("../middlewares/auth");
 
 router.post("/signup", createUser);
 router.post("/signin", login);
 router.use("/items", clothingItems);
-router.use("/users", users, auth, userRouter);
+router.use("/users", auth, userRouter);
 router.use((req, res) => {
   res.status(NOT_FOUND).send({ message: "Router not found" });
 });
