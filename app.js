418d0a0155172ce5c011061483f71f2c7a2db989
Missy <mhyatt91@yahoo.com>
2026-06-12 10:18:14 -0400

    Marina Email Revisions

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const mongoose = require("mongoose");
-const userRouter = require("express").Router();
-const itemRouter = require("express").Router();
 const mainRouter = require("./routes/index");
-const auth = require("./middlewares/auth");
-const { login, createUser } = require("./controllers/users");
 
 const app = express();
 const { PORT = 3001 } = process.env;
@@ -19,9 +15,5 @@ mongoose
   .catch(console.error);
 
 app.use(express.json());
-app.use("/users", auth, userRouter);
-app.use("/items", auth, itemRouter);
 app.use("/", mainRouter);
-app.post("signin", login);
-app.post("signup", createUser);
 app.listen(PORT, () => {});
