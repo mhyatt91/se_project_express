a63b3d1ad8a25c0e4b3b79fdc9d0f6c1075dc37c
Missy <mhyatt91@yahoo.com>
2026-08-12 10:44:55 -0400

    Final Project 15 update

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
-
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
 const errorHandler = require("../se_project_express/middlewares/errorHandler");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
 const app = express();
 const { PORT = 3001 } = process.env;
 
 app.use(cors());
+
+app.use(requestLogger);
+app.use(routes);
+
+app.use(errorLogger); // enabling the error logger
+
+app.use(errors()); // celebrate error handler
+app.use(errorHandler);
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/wtwr_db")
