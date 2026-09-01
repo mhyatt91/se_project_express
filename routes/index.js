644fb228d9ce16f7601c4258b834b9ba9a15cef0
Missy <mhyatt91@yahoo.com>
2026-09-01 11:01:02 -0400

    ssh error

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,14 @@ const clothingItems = require("./clothingItems");
 const { NOT_FOUND } = require("../utils/errors");
 const userRouter = require("./users");
 const auth = require("../middlewares/auth");
+const NotFoundError = require("../utils/errors/not-found-err");
 
 router.post("/signup", createUser);
 router.post("/signin", login);
 router.use("/items", clothingItems);
 router.use("/users", auth, userRouter);
 router.use((req, res, next) => {
-  return next(new NotFound("User not found"));
+  return next(new NotFoundError("User not found"));
 });
 
 module.exports = router;
